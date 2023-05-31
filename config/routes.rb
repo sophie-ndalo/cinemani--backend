2d01dd0ee08f6d55475781cd968679d78945f0d1
sophia <sophiendalo@gmail.com>
2023-05-31 16:49:28 +0300

    1. Update routes.rb/ created login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
+
   resources :movies
   resources :users
   resources :directors
