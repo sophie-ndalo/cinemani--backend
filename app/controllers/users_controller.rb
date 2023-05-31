f04b01dff7ead917c9c3f72d5dd9d945a81e7229
sophia <sophiendalo@gmail.com>
2023-05-31 16:13:38 +0300

    1. Update Users controller/ create method and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-  
-    if @user.save
-      session[:user_id] = @user.id
-      render json: { message: "User was successfully created." }
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
     else
-      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
     end
   end
-  
-
- 
-  
-  
-  
-  
-  
   
   def show
     @user = User.find_by(id: session[:user_id])
@@ -35,8 +25,9 @@ class UsersController < ApplicationController
 
   private
 
+  private
   def user_params
-    params.permit(:name, :email, :password)
+    params.require(:user).permit(:username, :password, :bio, :avatar)
   end
 end
 
